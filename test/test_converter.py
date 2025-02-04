50d94e0176760276a873c2921869209a917f700e
samrizee <samrawitzerfu@gmail.com>
2025-02-04 12:38:13 +0300

    changes to test_converter file

--- test/test_converter.py
+++ test/test_converter.py
@@ -4,7 +4,7 @@ class TestConverter(unittest.TestCase):
     def test_conversion(self):
         self.assertEqual(celisus_to_farhaniet(0), 32)
         self.assertEqual(celisus_to_farhaniet(100), 212)
-        self.assertEqual(celisus_to_farhaniet(25.5), 77.9, places=2)
+        self.assertAlmostEqual(celisus_to_farhaniet(25.5), 77.9, places=2)
 
 if __name__ == '__main__':
     unittest.main()

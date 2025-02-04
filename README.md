fb294ee908121087974d8e7047bfc60d0d2ffc48
samrizee <samrawitzerfu@gmail.com>
2025-02-04 11:26:34 +0300

    new features added

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ A simple Python package fro converting tempratures between celsius and  farhanit
 Clone the repository and Navigate into the Directory 
 
 ```bash
-$ git clone <URL>
+$ git clone https://github.com/Samrizee/temperature_converter.git
 $cd temprature_converter
 $pip install -e .
 ```
@@ -18,11 +18,10 @@ Import the conversion functions
 
 ```python
 from temprature_converter.converter import celsius_to_farhanite, farhanite_to_celcius
-print (cesius_to_farhanite(25))
+print (celisus_to_farhaniet(26))
 print (farhanite to celsius(77))
 ```
-
-##Running Unit test
+## Running Unit test
 
 To run tests, navigate to the test directory and run:
 
@@ -31,7 +30,23 @@ $ python -m unittest test_converter.py
 ```
 
 
- 
-# temperature_converter
-# temperature_converter
+## CLI usage
 
+To Use this temprature converter from Command-Line Interface, go to the root folder, temperature_converter
+
+```bash
+
+cd /Pypi/temperature_converter
+python -m temperature_converter.cli 100 Celsius Fahrenheit -- this will convert 100 Celsius to Fahrenheit
+python -m temperature_converter.cli 10 Celsius Kelvin -- this will convert 10 Celsius to Kelvin
+python -m temperature_converter.cli 30 Fahrenheit Kelvin -- this will convert 30 Fahrenheit to Kelvin
+python -m temperature_converter.cli 50 Fahrenheit Celsius -- this will convert 50 Fahrenheit to Celsius
+python -m temperature_converter.cli 200 Kelvin Fahrenheit -- this will convert 200 Kelvin to Fahrenheit
+python -m temperature_converter.cli 300 Kelvin Celsius -- this will convert  300 Kelvin to Celsius
+
+```
+
+
+
+
+ 
--- temperature_converter/converter.py
+++ temperature_converter/converter.py
@@ -1,10 +1,14 @@
+from .logger import log_conversion
+
 def celisus_to_farhaniet(celsius):
     """
     this is a function that converts
     temprature form celsius scale to farhanite scale
     """
     fahr = (celsius *9/5) + 32
+    log_conversion(celsius,'Celsius',fahr, 'Fahrenheit')
     return fahr
+
 def farhanite_to_celsius(fahr):
    """
    this is a function that converts
@@ -15,4 +19,43 @@ def farhanite_to_celsius(fahr):
     
    """
    celsius= (fahr -32) *5/9
-   return celsius 
+   log_conversion(fahr,'Fahrenheit',celsius, 'Celsius')
+   return celsius
+
+def celisus_to_Kelvin(celisus):
+    """
+    this is a fucntion that converts celisus to Kelvin.
+    Example:
+     >>celisus_to_Kelvin(0)
+     >> 273.15
+     """
+    Kelvin= celisus + 273.15
+    log_conversion(celisus,'Celsius',Kelvin, 'Kelvin')
+    return Kelvin
+
+def Kelvin_to_Celisus(Kelvin):
+    """
+    This function converts Kelvin to Celisus.
+    Example
+    >>Kelvin_to_Celisus(273.15)
+    >>0
+    """
+    celisus= Kelvin -273.15
+    log_conversion(Kelvin,'Kelvin',celisus, 'Celsius')
+    return celisus
+
+def Kelvin_to_Fahrenheit(Kelvin):
+    """
+    This function changes Kelvin to Fahrenheit
+    """
+    Fahrenheit = ((Kelvin- 273.15) * (9/5)) + 32
+    log_conversion(Kelvin,'Kelvin',Fahrenheit, 'Fahrenheit')
+    return Fahrenheit
+
+def Fahrenheit_to_Kelvin(Fahrenheit):
+    """
+    This function is usd to convert Farhenhit to KElvin scale
+    """
+    Kelvin = ((Fahrenheit - 32) * (5/9)) + 273.15
+    log_conversion(Fahrenheit,'Fahrenheit',Kelvin, 'Kelvin')
+    return Kelvin
